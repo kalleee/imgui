cc6e0ff3310a7f80300308de8a7e03830c81d89c
Kern Pascal <kern.pasca@gmail.com>
2019-11-07 14:39:58 +0100

    Changed premake file

--- premake5.lua
+++ premake5.lua
@@ -4,31 +4,38 @@
 --- DateTime: 04.11.19 19:55
 ---
 project "ImGui"
-kind "StaticLib"
-language "C++"
-cppdialect "C++17"
-staticruntime "on"
+	kind "StaticLib"
+	language "C++"
 
-targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-files{
-    "imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw",
-    "imgui_internal",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit",
-    "imstb_truetype",
-    "imgui_demo.cpp",
-}
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
 
-filter "system:windows"
-systemversion "latest"
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
 
-filter "configurations:Debug"
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
